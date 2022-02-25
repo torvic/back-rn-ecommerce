1b9fb2baf6ef290ee53f77c98ba97b1cd21ee712
torvic <130111@unsaac.edu.pe>
2022-02-24 20:13:02 -0500

    Add new resolvers and typeDefs to GraphQL

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,13 +1,21 @@
-const { getAllProductsHandler } = require('./product/product.controller');
+const { addNewOrderItemHandler, getAllOrderItemNotCompleteHandler } = require('./orderItem/orderItem.controller');
+const {
+  getAllProductsHandler, addNewProductHandler, updateProductHandler, deleteProductHandler,
+} = require('./product/product.controller');
 const { createAccountHandler, logInUserHandler } = require('./user/user.controller');
 
 const resolvers = {
   Query: {
     allProducts: getAllProductsHandler,
+    getAllOrderItemNotComplete: getAllOrderItemNotCompleteHandler,
   },
   Mutation: {
     createAccount: createAccountHandler,
     logInUser: logInUserHandler,
+    addProduct: addNewProductHandler,
+    updateProduct: updateProductHandler,
+    deleteProduct: deleteProductHandler,
+    addNewOrderItem: addNewOrderItemHandler,
   },
 };
 
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,6 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
+  type Order {
+    _id: ID!
+    address: String
+    complete: Boolean!
+    transactionId: Float
+    user: ID!
+  }
+
   type Product {
     _id: ID!
     title: String!
@@ -17,6 +26,16 @@ const typeDefs = gql`
     updatedAt: String
   }
 
+  type OrderItem {
+    _id: ID!
+    quantity: Int!
+    option: String
+    product: Product!
+    order: Order!
+    createdAt: String
+    updatedAt: String
+  }
+
   type User {
     _id: ID!
     email: String!
@@ -27,7 +46,10 @@ const typeDefs = gql`
   }
 
   type Query {
+    "Get all products"
     allProducts: [Product]
+    "Get all order items not complete"
+    getAllOrderItemNotComplete: [OrderItem]
   }
 
   input CreateUserInput {
@@ -41,9 +63,55 @@ const typeDefs = gql`
     password: String!
   }
 
+  input ProductInput {
+    title: String!
+    description: String!
+    image: String!
+    images: [String!]!
+    options: [String!]
+    avgRating: Float!
+    ratings: Float
+    price: Float!
+    oldPrice: Float
+  }
+
+  input UpdateProductInput {
+    id: ID!
+    title: String
+    description: String
+    image: String
+    images: [String]
+    options: [String]
+    avgRating: Float
+    ratings: Float
+    price: Float
+    oldPrice: Float
+  }
+
+  input OrderItemInput {
+    quantity: Int!
+    option: String
+    product: ID!
+  }
+
   type LoginPayload {
     user: User
     token: String!
+  }
+
+  type DeleteProductPayload {
+    product: Product
+    message: String!
+  }
+
+  type OrderItemPayload {
+    _id: ID!
+    quantity: Int!
+    option: String
+    product: ID!
+    order: ID!
+    createdAt: String
+    updatedAt: String
   }
 
   type Mutation {
@@ -51,6 +119,14 @@ const typeDefs = gql`
     createAccount(input: CreateUserInput!): User
     "Login an user"
     logInUser(input: LoginUserInput!): LoginPayload!
+    "Create a product"
+    addProduct(input: ProductInput!): Product
+    "Update a product"
+    updateProduct(input: UpdateProductInput!): Product
+    "Delete a product"
+    deleteProduct(id: ID!): DeleteProductPayload!
+    "Create an order item"
+    addNewOrderItem(input: OrderItemInput!): OrderItemPayload
   }
 `;
 

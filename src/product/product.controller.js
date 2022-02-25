d6fc0745b5c9847fbd19cfa82fe3ddb09c081df1
torvic <130111@unsaac.edu.pe>
2022-02-24 20:12:26 -0500

    Add product service and controller

--- src/product/product.controller.js
+++ src/product/product.controller.js
@@ -1,10 +1,35 @@
-const { getAllProducts } = require('./product.service');
+const {
+  getAllProducts, addNewProduct, updateProduct, deleteProduct,
+} = require('./product.service');
+
+async function addNewProductHandler(parent, args) {
+  // validate with context
+  const newProduct = args.input;
+  const product = await addNewProduct(newProduct);
+  return product;
+}
 
 async function getAllProductsHandler() {
   const products = await getAllProducts();
   return products;
 }
 
+async function updateProductHandler(_, args) {
+  const { id } = args.input;
+  const info = args.input;
+  const product = await updateProduct(id, info);
+  return product;
+}
+
+async function deleteProductHandler(_, args) {
+  const { id } = args;
+  const product = await deleteProduct(id);
+  return { message: 'Product deleted successfully', product };
+}
+
 module.exports = {
   getAllProductsHandler,
+  addNewProductHandler,
+  updateProductHandler,
+  deleteProductHandler,
 };
--- src/product/product.service.js
+++ src/product/product.service.js
@@ -4,6 +4,21 @@ function getAllProducts() {
   return Product.find();
 }
 
+function addNewProduct(product) {
+  return Product.create(product);
+}
+
+function updateProduct(id, info) {
+  return Product.findByIdAndUpdate(id, info, { new: true });
+}
+
+function deleteProduct(id) {
+  return Product.findByIdAndDelete(id);
+}
+
 module.exports = {
   getAllProducts,
+  addNewProduct,
+  updateProduct,
+  deleteProduct,
 };
